1fe3e69d5e892b4e56a98cc7f9170b9d4400ee88
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-10-29 11:43:22 +0330

    Add project files.

---
+++ OnlineStoreCore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineStoreCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
