ff4d060a1980b8929b06dd9118455142d426e5cc
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-10-29 12:49:56 +0330

    Set up base data models for project: Cart, CartItem, Category, Item, and Product.

---
+++ OnlineStoreCore/Models/CartItem.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineStoreCore.Models
+{
+    public class CartItem
+    {
+        public string Id { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public decimal getTotalPrice()
+        {
+            return Item.Price = Quantity;
+        }
+    }
+}
---
+++ OnlineStoreCore/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStoreCore.Models
+{
+    public class Product
+    {
+        public Product() { Categories = new List<Category>(); }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
