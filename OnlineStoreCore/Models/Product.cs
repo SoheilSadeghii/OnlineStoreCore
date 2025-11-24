a922b06211d0ec46b524b50dd7a0e1a912c59d5e
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-11-24 13:25:05 +0330

    Define the relationship between Item and Product models.

--- OnlineStoreCore/Models/Product.cs
+++ OnlineStoreCore/Models/Product.cs
@@ -5,7 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public int ItemId { get; set; }
 
         public ICollection<CategoryToProduct> CategoryToProducts { get; set; }
+        public Item Item { get; set; }
     }
 }
