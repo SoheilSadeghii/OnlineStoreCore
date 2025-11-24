cb416dcd14a3a2d3eee18fca5561bc15b6368214
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2025-11-24 13:08:05 +0330

    Added CategoryToProduct to define the relationship between Category and Product models.

--- OnlineStoreCore/Models/Product.cs
+++ OnlineStoreCore/Models/Product.cs
@@ -2,10 +2,10 @@
 {
     public class Product
     {
-        public Product() { Categories = new List<Category>(); }
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public List<Category> Categories { get; set; }
+
+        public ICollection<CategoryToProduct> CategoryToProducts { get; set; }
     }
 }
