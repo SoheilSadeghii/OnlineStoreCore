52b020f2ace0cbc3a96a22ae7cce89e1b12c5257
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-01-16 19:59:37 +0330

    feat: added view model for product and categories

---
+++ OnlineStoreCore/Models/DetailsViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OnlineStoreCore.Models
+{
+    public class DetailsViewModel
+    {
+        public Product Product { get; set; }
+        public List<CategoryToProduct> Categories { get; set; }
+    }
+}
