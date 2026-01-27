722ce8167cca4da3d7359147fcebc70cf4e1eda1
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-01-27 18:47:04 +0330

    feat: add component for categories

---
+++ OnlineStoreCore/Components/ProductGroupsComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStoreCore.Data;
+
+namespace OnlineStoreCore.Components
+{
+    public class ProductGroupsComponent : ViewComponent
+    {
+        private OnlineStoreCoreContext _context;
+
+        public ProductGroupsComponent(OnlineStoreCoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(_context.Categories);
+        }
+    }
+}
