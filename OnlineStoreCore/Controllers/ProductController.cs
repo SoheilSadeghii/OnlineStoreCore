d1a408fd8df0092cff625fb39c3c3b26ba21d45f
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-01-31 22:38:04 +0330

    feat: create product controller

---
+++ OnlineStoreCore/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStoreCore.Data;
+
+namespace OnlineStoreCore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private OnlineStoreCoreContext _context;
+        public ProductController(OnlineStoreCoreContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
