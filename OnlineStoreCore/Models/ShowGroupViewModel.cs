a4155f5e84ffd6fff369d7ab487be50e2fdb6b8d
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-01 20:36:39 +0330

    feat: add new view model for show group

---
+++ OnlineStoreCore/Models/ShowGroupViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStoreCore.Models
+{
+    public class ShowGroupViewModel
+    {
+        public int GroupId { get; set; }
+        public string Name { get; set; }
+        public int ProductCount { get; set; }
+    }
+}
