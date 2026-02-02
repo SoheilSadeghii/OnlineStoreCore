ab5c259d1f5c9f2b22297aaa0459d925aeedc286
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-02 20:30:15 +0330

    feat: add repository to project

---
+++ OnlineStoreCore/Repositories/IGroupRepository.cs
@@ -0,0 +1,23 @@
+﻿using OnlineStoreCore.Models;
+
+namespace OnlineStoreCore.Repositories
+{
+    public interface IGroupRepository
+    {
+        public List<Category> GetAllCategories();
+        public List<CategoryToProduct> GetGroupForShow();
+    }
+
+    public class GroupRepository : IGroupRepository
+    {
+        public List<Category> GetAllCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<CategoryToProduct> GetGroupForShow()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
