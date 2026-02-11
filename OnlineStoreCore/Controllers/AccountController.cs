13340c42345bdfa361162804a51587c191c42c7b
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-11 22:10:51 +0330

    feat: add validation scripts partial for validate input in client section

--- OnlineStoreCore/Controllers/AccountController.cs
+++ OnlineStoreCore/Controllers/AccountController.cs
@@ -1,11 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStoreCore.Models;
 
 namespace OnlineStoreCore.Controllers
 {
     public class AccountController : Controller
     {
+        [HttpGet]
         public IActionResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel register)
+        {
+            if (!ModelState.IsValid) { return View(register); }
+
             return View();
         }
     }
