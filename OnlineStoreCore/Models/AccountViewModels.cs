209623e076eb2743e6dc2cf444e2c05d68a86b4e
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-11 21:00:35 +0330

    feat: add viewmodel and define register property

---
+++ OnlineStoreCore/Models/AccountViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStoreCore.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [MaxLength(300)]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string RePassword { get; set; }
+    }
+}
