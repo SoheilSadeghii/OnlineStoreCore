049a28c79efc3da0e29b85b8041d11b47a5cd9ab
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-11 21:07:21 +0330

    feat: add controller for register

---
+++ OnlineStoreCore/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStoreCore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
