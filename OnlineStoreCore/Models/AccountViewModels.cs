45ff759d4e64674c3e5e152b7f4f22909849d34c
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-11 21:29:53 +0330

    feat: add display for each field

--- OnlineStoreCore/Models/AccountViewModels.cs
+++ OnlineStoreCore/Models/AccountViewModels.cs
@@ -7,15 +7,18 @@ namespace OnlineStoreCore.Models
         [Required]
         [MaxLength(300)]
         [EmailAddress]
+        [Display(Name = "ایمیل")]
         public string Email { get; set; }
         [Required]
         [MaxLength(50)]
         [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور")]
         public string Password { get; set; }
         [Required]
         [MaxLength(50)]
         [DataType(DataType.Password)]
         [Compare("Password")]
+        [Display(Name = "تکرار کلمه عبور")]
         public string RePassword { get; set; }
     }
 }
