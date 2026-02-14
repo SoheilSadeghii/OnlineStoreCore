01432229cb801bd722d528bcf70025c8cfb2c494
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-14 22:10:32 +0330

    feat: add repository of the register

---
+++ OnlineStoreCore/Data/Repositories/IUserRepository.cs
@@ -0,0 +1,29 @@
+﻿using OnlineStoreCore.Models;
+
+namespace OnlineStoreCore.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        void AddUser(User user);
+        bool IsExistUserByEmail(string email);
+    }
+
+    public class UserRepository : IUserRepository
+    {
+        private OnlineStoreCoreContext _context;
+        public UserRepository(OnlineStoreCoreContext context)
+        {
+            _context = context;
+        }
+        public void AddUser(User user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+        }
+
+        public bool IsExistUserByEmail(string email)
+        {
+            return _context.Users.Any(e => e.Email == email);
+        }
+    }
+}
