63933fd7f67d9e2fff4fdec6a473626b3281f895
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-14 22:15:23 +0330

    feat: add repository to controller

--- OnlineStoreCore/Controllers/AccountController.cs
+++ OnlineStoreCore/Controllers/AccountController.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStoreCore.Data.Repositories;
 using OnlineStoreCore.Models;
 
 namespace OnlineStoreCore.Controllers
 {
     public class AccountController : Controller
     {
+        private IUserRepository _userRepository;
+        public AccountController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
         [HttpGet]
         public IActionResult Register()
         {
