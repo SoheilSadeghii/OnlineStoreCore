fb284b30664e5d7a907bb6a409400409a23c9af0
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-15 20:33:42 +0330

    feat: add view model of login

--- OnlineStoreCore/Models/AccountViewModels.cs
+++ OnlineStoreCore/Models/AccountViewModels.cs
@@ -7,9 +7,9 @@ namespace OnlineStoreCore.Models
     {
         [Required(ErrorMessage = "لطفا {0} را وارد کنید!")]
         [MaxLength(300)]
-        [Display(Name ="نام و نام خانوادگی")]
+        [Display(Name = "نام و نام خانوادگی")]
         public string FullName { get; set; }
-        [Required(ErrorMessage ="لطفا {0} را وارد کنید!")]
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید!")]
         [MaxLength(300)]
         [EmailAddress]
         [Display(Name = "ایمیل")]
@@ -25,5 +25,22 @@ namespace OnlineStoreCore.Models
         [Compare("Password")]
         [Display(Name = "تکرار کلمه عبور")]
         public string RePassword { get; set; }
+    }
+
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید!")]
+        [MaxLength(300)]
+        [EmailAddress]
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "لطفا {0} را وارد کنید!")]
+        [MaxLength(50)]
+        [DataType(DataType.Password)]
+        [Display(Name = "کلمه عبور")]
+        public string Password { get; set; }
+
+        [Display(Name = "مرا به خاطر بسپار")]
+        public bool RememberMe { get; set; }
     }
 }
