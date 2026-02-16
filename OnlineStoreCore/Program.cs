9f9e303eabef13ffa7cfed27796492e1ea74156f
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-16 22:09:33 +0330

    feat: configure cookie authentication with login/logout paths and 1-day expiration

--- OnlineStoreCore/Program.cs
+++ OnlineStoreCore/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.Web.CodeGeneration.EntityFrameworkCore;
 using OnlineStoreCore.Data;
@@ -24,6 +25,18 @@ builder.Services.AddScoped<IUserRepository, UserRepository>();
 
 #endregion
 
+#region Authentication
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(option =>
+    {
+        option.LoginPath = "/Account/Login";
+        option.LogoutPath = "Account/Logout";
+        option.ExpireTimeSpan = TimeSpan.FromDays(1);
+    });
+
+#endregion
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,6 +45,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
 }
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.UseAuthorization();
 
