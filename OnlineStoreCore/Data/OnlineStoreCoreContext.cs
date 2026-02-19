d8124fcb155bf3add28be1de61290f54d14f3900
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-19 20:59:40 +0330

    feat: add db sets of the models that defined

--- OnlineStoreCore/Data/OnlineStoreCoreContext.cs
+++ OnlineStoreCore/Data/OnlineStoreCoreContext.cs
@@ -15,6 +15,8 @@ namespace OnlineStoreCore.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
