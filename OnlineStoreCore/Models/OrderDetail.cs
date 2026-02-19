eabb3bf6c716b91ed130fbb07a36e0b60678fe84
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-19 20:57:49 +0330

    feat: add price to model and give data annotation

--- OnlineStoreCore/Models/OrderDetail.cs
+++ OnlineStoreCore/Models/OrderDetail.cs
@@ -1,12 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OnlineStoreCore.Models
 {
     public class OrderDetail
     {
+        [Key]
         public int DetailId { get; set; }
+        [Required]
         public int OrderId { get; set; }
+        [Required]
         public int ProductId { get; set; }
+        [Required]
+        public decimal Price { get; set; }
 
 
         // Navigation Property
