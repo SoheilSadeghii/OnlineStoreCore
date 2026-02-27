2ad10acbafa9f74461d9acac5ab651284e63ec13
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-02-27 15:31:24 +0330

    chor: add count to order detail table

--- OnlineStoreCore/Migrations/20260227115509_CreateInit3.Designer.cs
+++ OnlineStoreCore/Migrations/20260227115509_CreateInit3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OnlineStoreCore.Data;
 
@@ -11,9 +12,11 @@ using OnlineStoreCore.Data;
 namespace OnlineStoreCore.Migrations
 {
     [DbContext(typeof(OnlineStoreCoreContext))]
-    partial class OnlineStoreCoreContextModelSnapshot : ModelSnapshot
+    [Migration("20260227115509_CreateInit3")]
+    partial class CreateInit3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
