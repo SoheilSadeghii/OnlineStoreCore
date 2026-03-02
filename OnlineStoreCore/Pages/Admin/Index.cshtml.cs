93988d7d8547ce4fa0bf630503aa6a0d53a7490d
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-03-02 23:25:53 +0330

    feat: complete get section in index

--- OnlineStoreCore/Pages/Admin/Index.cshtml.cs
+++ OnlineStoreCore/Pages/Admin/Index.cshtml.cs
@@ -1,12 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OnlineStoreCore.Data;
+using OnlineStoreCore.Models;
 
 namespace OnlineStoreCore.Pages.Admin
 {
     public class IndexModel : PageModel
     {
+        private OnlineStoreCoreContext _context;
+        public IndexModel(OnlineStoreCoreContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Product> Products { get; set; }
         public void OnGet()
         {
+            Products = _context.Products.Include(p => p.Item);
         }
+        public void OnPost() { }
     }
 }
