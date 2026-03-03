914fc1fde50ad1828c0cc5bf968b5b51a64723a5
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-03-03 13:22:48 +0330

    feat: add fields and inputs of the add or edit products

--- OnlineStoreCore/Pages/Admin/Add.cshtml.cs
+++ OnlineStoreCore/Pages/Admin/Add.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using OnlineStoreCore.Models;
 
 namespace OnlineStoreCore.Pages.Admin
 {
     public class AddModel : PageModel
     {
+        [BindProperty]
+        public AddEditProductViewModel Product { get; set; }
         public void OnGet()
         {
+
         }
     }
 }
