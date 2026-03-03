6787a822d2c412218378a0d5266fa80c6bebedc1
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-03-03 21:11:09 +0330

    feat: complet edit section and admin can edit products

--- OnlineStoreCore/Pages/Admin/Edit.cshtml.cs
+++ OnlineStoreCore/Pages/Admin/Edit.cshtml.cs
@@ -18,7 +18,7 @@ namespace OnlineStoreCore.Pages.Admin
         public AddEditProductViewModel Product { get; set; }
         public void OnGet(int id)
         {
-            Product = _context.Products.Include(p => p.Item)
+            var product = _context.Products.Include(p => p.Item)
                 .Where(p => p.Id == id)
                 .Select(s => new AddEditProductViewModel()
                 {
@@ -29,6 +29,37 @@ namespace OnlineStoreCore.Pages.Admin
                     Price = s.Item.Price
                 }).FirstOrDefault();
 
+            Product = product;
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var product = _context.Products.Find(Product.Id);
+            var item = _context.Items.First(i => i.Id == product.ItemId);
+
+            product.Name = Product.Name;
+            product.Description = Product.Description;
+
+            item.Price = Product.Price;
+            item.QuantityInStock = Product.QuantityInStock;
+
+            _context.SaveChanges();
+
+            if (Product.Picture?.Length > 0)
+            {
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(),
+                    "wwwroot",
+                    "images",
+                    product.Id + Path.GetExtension(Product.Picture.FileName));
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    Product.Picture.CopyTo(stream);
+                }
+            }
+
+            return RedirectToAction("Index");
         }
     }
 }
