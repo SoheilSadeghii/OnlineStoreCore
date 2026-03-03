97c5026bd02ac71efe08e6a2103efb25167d98b5
Soheil Sadeghi <sohel.sadeghi.2003@gmail.com>
2026-03-03 14:18:35 +0330

    feat: complet add product and added images folder to git ignore

--- OnlineStoreCore/Pages/Admin/Add.cshtml.cs
+++ OnlineStoreCore/Pages/Admin/Add.cshtml.cs
@@ -1,16 +1,62 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using OnlineStoreCore.Data;
 using OnlineStoreCore.Models;
 
 namespace OnlineStoreCore.Pages.Admin
 {
     public class AddModel : PageModel
     {
+        private OnlineStoreCoreContext _context;
+        public AddModel(OnlineStoreCoreContext context)
+        {
+            _context = context;
+        }
+
         [BindProperty]
         public AddEditProductViewModel Product { get; set; }
         public void OnGet()
         {
 
+        }
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            var item = new Item()
+            {
+                Price = Product.Price,
+                QuantityInStock = Product.QuantityInStock
+            };
+            _context.Add(item);
+            _context.SaveChanges();
+
+            var product = new Product()
+            {
+                Name = Product.Name,
+                Item = item,
+                Description = Product.Description
+            };
+            _context.Add(product);
+            _context.SaveChanges();
+            product.ItemId = item.Id;
+            _context.SaveChanges();
+
+            if (Product.Picture?.Length > 0)
+            {
+                string filePath = Path.Combine(Directory.GetCurrentDirectory(),
+                    "wwwroot",
+                    "images",
+                    product.Id + Path.GetExtension(Product.Picture.FileName));
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    Product.Picture.CopyTo(stream);
+                }
+            }
+
+            return RedirectToPage("Index");
         }
     }
 }
